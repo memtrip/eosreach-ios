4a4fb915f7dbf92dc96e76d019df4ec597786602
Samuel Kirton <samkirton@me.com>
2019-02-11 09:23:20 +0000

    Added a test for proxy voter list information

---
+++ eosreachTests/app/account/vote/ProxyVoteInformationTestCase.swift
@@ -0,0 +1,36 @@
+import Foundation
+@testable import stub
+
+class ProxyVoteInformationTestCase : TestCase {
+
+    func testProxyVoteInformation() {
+        
+        importKeyOrchestra
+            .go(privateKey: "5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3")
+        
+        accountRobot.begin { it in
+            it.verifyAccountScreen()
+            it.verifyAvailableBalance(availableBalance: "$162947.12")
+            it.selectVoteTab()
+        }
+        
+        voteRobot.begin { it in
+            it.verifyNotVotedScreen()
+            it.selectVoteForProxy()
+            it.verifyCastProxyVoteScreen()
+            it.selectExploreProxyAccountsButton()
+        }
+        
+        proxyRobot.begin { it in
+            it.verifyExploreProxyAccounts()
+            it.selectFirstProxyInformationRow()
+            it.verifyProxyInformation()
+            it.selectViewAccount()
+        }
+        
+        accountRobot.begin { it in
+            it.verifyAccountScreen()
+            it.verifyAvailableBalance(availableBalance: "$162947.12")
+        }
+    }
+}
--- eosreachTests/robot/ProxyRobot.swift
+++ eosreachTests/robot/ProxyRobot.swift
@@ -22,4 +22,58 @@ class ProxyRobot: Robot {
             .matchesNext(isDisplayed())
             .perform(click())
     }
+    
+    func selectFirstProxyInformationRow() {
+        
+        onView(
+            withIdInParent(
+                withIdInTableView("proxy_voter_list_tableview", position: 0, id: "proxy_voter_list_border"),
+                id: "proxy_voter_list_information_button"
+            )
+        )
+            .matchesNext(isDisplayed())
+            .perform(click())
+    }
+    
+    func verifyProxyInformation() {
+        
+        onView(withId("view_proxy_voter_toolbar"))
+            .matches(isDisplayed())
+        
+        onView(withId("view_proxy_voter_container"))
+            .matches(isDisplayed())
+        
+        onView(allOf(
+            withId("view_proxy_voter_image"),
+            ancestorOf(withId("view_proxy_voter_container"))
+        )).matches(isDisplayed())
+        
+        onView(allOf(
+            withId("view_proxy_voter_blurb"),
+            ancestorOf(withId("view_proxy_voter_container"))
+        )).matches(isDisplayed())
+        
+        onView(allOf(
+            withId("view_proxy_voter_website"),
+            ancestorOf(withId("view_proxy_voter_container"))
+        )).matches(isDisplayed())
+        
+        onView(allOf(
+            withId("view_proxy_voter_view_account"),
+            ancestorOf(withId("view_proxy_voter_container"))
+        )).matches(isDisplayed())
+        
+        onView(allOf(
+            withId("view_proxy_voter_summary"),
+            ancestorOf(withId("view_proxy_voter_container"))
+        )).matches(isDisplayed())
+    }
+    
+    func selectViewAccount() {
+       
+        onView(allOf(
+            withId("view_proxy_voter_view_account"),
+            ancestorOf(withId("view_proxy_voter_container"))
+        )).matchesNext(isDisplayed()).perform(click())
+    }
 }
