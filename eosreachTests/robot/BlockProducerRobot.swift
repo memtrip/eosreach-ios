fc5b8d97f8ad11ed387753b63dde8becafdd0844
Samuel Kirton <samkirton@me.com>
2019-02-11 06:49:03 +0000

    View block producer from vote screen test case

--- eosreachTests/robot/BlockProducerRobot.swift
+++ eosreachTests/robot/BlockProducerRobot.swift
@@ -3,15 +3,42 @@ import Foundation
 class BlockProducerRobot: Robot {
  
     func verifyBlockProducerListScreen() {
+        onView(withId("block_producer_list_toolbar"))
+            .matches(isDisplayed())
         
+        onView(withId("block_producer_list_tableview"))
+            .matches(isDisplayed())
     }
     
     func verifyBlockProducerListFirstAccountRow() {
+        onView(
+            withIdInParent(
+                withIdInTableView("block_producer_list_tableview", position: 0, id: "block_producer_list_cell_border"),
+                id: "block_producer_list_cell_owner"
+            )
+        )
+            .matchesNext(isDisplayed())
+            .matches(withText("eosnewyorkio"))
         
+        onView(
+            withIdInParent(
+                withIdInTableView("block_producer_list_tableview", position: 0, id: "block_producer_list_cell_border"),
+                id: "block_producer_list_cell_account_name"
+            )
+        )
+            .matchesNext(isDisplayed())
+            .matches(withText("EOS New York"))
     }
     
     func selectBlockProducerListFirstAccountRowInformation() {
-        
+        onView(
+            withIdInParent(
+                withIdInTableView("block_producer_list_tableview", position: 0, id: "block_producer_list_cell_border"),
+                id: "block_producer_list_cell_information_button"
+            )
+        )
+            .matchesNext(isDisplayed())
+            .perform(click())
     }
     
     func verifyViewBlockProducerInformationScreen(
@@ -19,10 +46,48 @@ class BlockProducerRobot: Robot {
         website: String,
         email: String
     ) {
+ 
+        onView(withId("view_block_producer_toolbar"))
+            .matches(isDisplayed())
         
+        onView(withId("view_block_producer_container"))
+            .matches(isDisplayed())
+        
+        onView(allOf(
+            withId("view_block_producer_icon"),
+            ancestorOf(withId("view_block_producer_container")))
+        ).matches(isDisplayed())
+        
+        onView(allOf(
+            withId("view_block_producer_website_button"),
+            ancestorOf(withId("view_block_producer_container")))
+        ).matches(isDisplayed())
+   
+        onView(allOf(
+            withId("view_block_producer_email_button"),
+            ancestorOf(withId("view_block_producer_container")))
+        ).matches(isDisplayed())
+        
+        onView(allOf(
+            withId("view_block_producer_owner_button"),
+            ancestorOf(withId("view_block_producer_container")))
+        ).matches(isDisplayed())
+
+        onView(allOf(
+            withId("view_block_producer_code_of_conduct_button"),
+            ancestorOf(withId("view_block_producer_container")))
+        ).matches(isDisplayed())
+        
+        onView(allOf(
+            withId("view_block_producer_ownership_button"),
+            ancestorOf(withId("view_block_producer_container")))
+        ).matches(isDisplayed())
     }
     
     func selectViewOwnerAccount() {
-        
+        onView(allOf(
+            withId("view_block_producer_owner_button"),
+            ancestorOf(withId("view_block_producer_container")))
+        ).matchesNext(isDisplayed()).perform(click())
     }
 }
