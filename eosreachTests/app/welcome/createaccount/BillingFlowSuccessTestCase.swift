8a453083922ad1460a950f59232517bc52f385bb
Samuel Kirton <samkirton@me.com>
2019-02-15 22:51:29 +0000

    Implemented the first create account test case

--- eosreachTests/app/welcome/createaccount/BillingFlowSuccessTestCase.swift
+++ eosreachTests/app/welcome/createaccount/BillingFlowSuccessTestCase.swift
@@ -4,6 +4,15 @@ import Foundation
 class BillingFlowSuccessTestCase : TestCase {
 
     func testBillingFlowSuccess() {
+        splashRobot.selectCreateAccount()
         
+        createAccountRobot.begin { it in
+            it.verifyEnterAccountNameScreen()
+            it.typeAccountName(accountName: "memtripissue")
+            it.verifyAccountCreatedScreen()
+            it.selectAccountCreatedDoneButton()
+        }
+        
+        accountRobot.verifyAccountScreen()
     }
 }
--- eosreachTests/robot/CreateAccountRobot.swift
+++ eosreachTests/robot/CreateAccountRobot.swift
@@ -2,4 +2,70 @@ import Foundation
 
 class CreateAccountRobot : Robot {
     
+    func verifyEnterAccountNameScreen() {
+        onView(withId("create_account_toolbar"))
+            .matches(isDisplayed())
+        
+        onView(withId("create_account_form"))
+            .matches(isDisplayed())
+        
+        onView(
+            withIdInParent(withId("create_account_form"), id: "create_account_name_input")
+        ).matches(isDisplayed())
+        
+        onView(
+            withIdInParent(withId("create_account_form"), id: "create_account_instruction_label")
+        ).matches(isDisplayed())
+        
+        onView(
+            withIdInParent(withId("create_account_form"), id: "create_account_resource_instruction_label")
+        ).matches(isDisplayed())
+        
+        onView(
+            withIdInParent(withId("create_account_form"), id: "create_account_form_cta")
+        ).matches(isDisplayed())
+    }
+    
+    func typeAccountName(accountName: String) {
+        
+        onView(
+            withIdInParent(withId("create_account_form"), id: "create_account_name_input")
+        ).matchesNext(isDisplayed()).perform(replaceText(accountName))
+    }
+    
+    func selectCreateAccountButton() {
+        
+        onView(
+            withIdInParent(withId("create_account_form"), id: "create_account_form_cta")
+        ).matchesNext(isDisplayed()).perform(click())
+    }
+    
+    func verifyAccountCreatedScreen() {
+        
+        onView(withId("create_account_done"))
+            .matches(isDisplayed())
+        
+        onView(
+            withIdInParent(withId("create_account_done"), id: "create_account_done_title_label")
+        ).matches(isDisplayed())
+        
+        onView(
+            withIdInParent(withId("create_account_done"), id: "create_account_done_instruction_label")
+        ).matches(isDisplayed())
+        
+        onView(
+            withIdInParent(withId("create_account_done"), id: "create_account_done_privatekey_label")
+        ).matches(isDisplayed())
+        
+        onView(
+            withIdInParent(withId("create_account_done"), id: "create_account_done_button")
+        ).matches(isDisplayed())
+    }
+    
+    func selectAccountCreatedDoneButton() {
+        
+        onView(
+            withIdInParent(withId("create_account_done"), id: "create_account_done_button")
+        ).matchesNext(isDisplayed()).perform(click())
+    }
 }
